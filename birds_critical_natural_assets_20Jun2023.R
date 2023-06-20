045b228002e87425c95297fb40aa560e5e6d6891
raenb0 <rachel.neugarten@gmail.com>
2023-06-20 14:31:52 -0400

    calculating percent of populations represented in CNA areas, by guild, and plotting results

--- birds_critical_natural_assets_20Jun2023.R
+++ birds_critical_natural_assets_20Jun2023.R
@@ -172,6 +172,7 @@ cerulean_warbler_sum_cna <- global(pct_pop_mask$cerwar, fun="sum", na.rm=TRUE)
 cerulean_warbler_sum_cna #0.9057386
 
 # calculate pct of population within CNA for individual spp ------------------
+library(tidyverse)
 
 #filter tipping point species, forest spp, etc.
 sps_vars_tipping_point <- sps_sel_all_vars %>%
@@ -184,6 +185,8 @@ sps_vars_aridland <- sps_sel_all_vars %>%
   filter(sps_groups=="Aridlands")
 sps_vars_wetland <- sps_sel_all_vars %>%
   filter(sps_groups=="Water/wetland")
+sps_vars_generalist <- sps_sel_all_vars %>%
+  filter(sps_groups=="Habitat Generalist")
 
 # pct pop of tipping point spp within CNA
 tipping_pt_spp <- unique(sps_vars_tipping_point$species_code) #unique tipping pt spp codes
@@ -193,31 +196,160 @@ spp_raster <- subset(pct_pop_mask, sel_species) #subset tipping pt spp rasters o
 pct_pop_tipping_pt_spp_cna <- global(spp_raster, fun='sum', na.rm=T) #calculate sum for tipping pt spp
 pct_pop_tipping_pt_spp_cna <- tibble::rownames_to_column(pct_pop_tipping_pt_spp_cna, "species_code")
 
-library(tidyverse)
 write_csv(pct_pop_tipping_pt_spp_cna, "outputs/pct_pop_tipping_pt_spp_cna.csv")
+
+# calculate pct of population within CNA for all forest spp ------------------
+#NOTE overwrites objects with same names
+
+forest_spp <- unique(sps_vars_forest$species_code) #unique forest spp codes
+raster_names <- names(pct_pop_mask) #masked to CNA
+sel_species <- raster_names[raster_names %in% forest_spp] #select forest spp
+spp_raster <- subset(pct_pop_mask, sel_species) #subset forest spp rasters only
+pct_pop_forest_spp_cna <- global(spp_raster, fun='sum', na.rm=T) #calculate sum for forest spp
+pct_pop_forest_spp_cna <- tibble::rownames_to_column(pct_pop_forest_spp_cna, "species_code")
+
+write_csv(pct_pop_forest_spp_cna, "outputs/pct_pop_forest_spp_cna.csv")
 
 # calculate pct of population within CNA for all Grassland spp ------------------
 #NOTE overwrites objects with same names
 
-grassland_spp <- unique(sps_vars_grassland$species_code) #unique tipping pt spp codes
+grassland_spp <- unique(sps_vars_grassland$species_code)
 raster_names <- names(pct_pop_mask) #masked to CNA
 sel_species <- raster_names[raster_names %in% grassland_spp] #select grassland spp
-spp_raster <- subset(pct_pop_mask, sel_species) #subset tipping pt spp rasters only
+spp_raster <- subset(pct_pop_mask, sel_species)
 pct_pop_grassland_spp_cna <- global(spp_raster, fun='sum', na.rm=T) #calculate sum for grassland spp
 pct_pop_grassland_spp_cna <- tibble::rownames_to_column(pct_pop_grassland_spp_cna, "species_code")
 
-library(tidyverse)
 write_csv(pct_pop_grassland_spp_cna, "outputs/pct_pop_grassland_spp_cna.csv")
 
 # calculate pct of population within CNA for all Aridland spp ------------------
 #NOTE overwrites objects with same names
 
-aridland_spp <- unique(sps_vars_aridland$species_code) #unique tipping pt spp codes
+aridland_spp <- unique(sps_vars_aridland$species_code)
 raster_names <- names(pct_pop_mask) #masked to CNA
-sel_species <- raster_names[raster_names %in% aridland_spp] #select grassland spp
-spp_raster <- subset(pct_pop_mask, sel_species) #subset tipping pt spp rasters only
-pct_pop_aridland_spp_cna <- global(spp_raster, fun='sum', na.rm=T) #calculate sum for aridland spp
+sel_species <- raster_names[raster_names %in% aridland_spp]
+spp_raster <- subset(pct_pop_mask, sel_species)
+pct_pop_aridland_spp_cna <- global(spp_raster, fun='sum', na.rm=T)
 pct_pop_aridland_spp_cna <- tibble::rownames_to_column(pct_pop_aridland_spp_cna, "species_code")
 
-library(tidyverse)
 write_csv(pct_pop_aridland_spp_cna, "outputs/pct_pop_aridland_spp_cna.csv")
+
+# calculate pct of population within CNA for all Wetland spp ------------------
+#NOTE overwrites objects with same names
+
+wetland_spp <- unique(sps_vars_wetland$species_code)
+raster_names <- names(pct_pop_mask) #masked to CNA
+sel_species <- raster_names[raster_names %in% wetland_spp]
+spp_raster <- subset(pct_pop_mask, sel_species)
+pct_pop_wetland_spp_cna <- global(spp_raster, fun='sum', na.rm=T)
+pct_pop_wetland_spp_cna <- tibble::rownames_to_column(pct_pop_wetland_spp_cna, "species_code")
+
+write_csv(pct_pop_wetland_spp_cna, "outputs/pct_pop_wetland_spp_cna.csv")
+
+# calculate pct of population within CNA for all Habitat Generalist spp ------------------
+#NOTE overwrites objects with same names
+
+generalist_spp <- unique(sps_vars_generalist$species_code)
+raster_names <- names(pct_pop_mask) #masked to CNA
+sel_species <- raster_names[raster_names %in% generalist_spp]
+spp_raster <- subset(pct_pop_mask, sel_species)
+pct_pop_generalist_spp_cna <- global(spp_raster, fun='sum', na.rm=T)
+pct_pop_generalist_spp_cna <- tibble::rownames_to_column(pct_pop_generalist_spp_cna, "species_code")
+
+write_csv(pct_pop_generalist_spp_cna, "outputs/pct_pop_generalist_spp_cna.csv")
+
+
+# calculate percent of spp that are >90 >75 >50 pct represented ---------------
+library(tidyverse)
+
+# load data if necessary
+pct_pop_tipping_pt_spp_cna <- read_csv("outputs/pct_pop_tipping_pt_spp_cna.csv")
+pct_pop_forest_spp_cna <- read_csv("outputs/pct_pop_forest_spp_cna.csv")
+pct_pop_grassland_spp_cna <- read_csv("outputs/pct_pop_grassland_spp_cna.csv")
+pct_pop_aridland_spp_cna <- read_csv("outputs/pct_pop_aridland_spp_cna.csv")
+pct_pop_wetland_spp_cna <- read_csv("outputs/pct_pop_wetland_spp_cna.csv")
+pct_pop_generalist_spp_cna <- read_csv("outputs/pct_pop_generalist_spp_cna.csv")
+
+# tipping point spp
+pct_pop_tipping_pt_spp_cna <- pct_pop_tipping_pt_spp_cna %>%
+  mutate(more90 = ifelse(sum>0.9,1,0)) %>%
+  mutate(more75 = ifelse(sum>0.75,1,0)) %>%
+  mutate(more50 = ifelse(sum>0.5,1,0))
+
+summary_tipping_pt_spp_cna <- pct_pop_tipping_pt_spp_cna %>%
+  summarize(more90 = mean(more90), more75 = mean(more75), more50 = mean(more50)) %>%
+  mutate(guild="tipping_point") %>%
+  mutate(n=nrow(pct_pop_tipping_pt_spp_cna))
+
+# forest spp
+pct_pop_forest_spp_cna <- pct_pop_forest_spp_cna %>%
+  mutate(more90 = ifelse(sum>0.9,1,0)) %>%
+  mutate(more75 = ifelse(sum>0.75,1,0)) %>%
+  mutate(more50 = ifelse(sum>0.5,1,0))
+
+summary_forest_spp_cna <- pct_pop_forest_spp_cna %>%
+  summarize(more90 = mean(more90), more75 = mean(more75), more50 = mean(more50)) %>%
+  mutate(guild="forest") %>%
+  mutate(n=nrow(pct_pop_forest_spp_cna))
+
+# grassland spp
+pct_pop_grassland_spp_cna <- pct_pop_grassland_spp_cna %>%
+  mutate(more90 = ifelse(sum>0.9,1,0)) %>%
+  mutate(more75 = ifelse(sum>0.75,1,0)) %>%
+  mutate(more50 = ifelse(sum>0.5,1,0))
+
+summary_grassland_spp_cna <- pct_pop_grassland_spp_cna %>%
+  summarize(more90 = mean(more90), more75 = mean(more75), more50 = mean(more50)) %>%
+  mutate(guild="grassland") %>%
+  mutate(n=nrow(pct_pop_grassland_spp_cna))
+
+# aridland spp
+pct_pop_aridland_spp_cna <- pct_pop_aridland_spp_cna %>%
+  mutate(more90 = ifelse(sum>0.9,1,0)) %>%
+  mutate(more75 = ifelse(sum>0.75,1,0)) %>%
+  mutate(more50 = ifelse(sum>0.5,1,0))
+
+summary_aridland_spp_cna <- pct_pop_aridland_spp_cna %>%
+  summarize(more90 = mean(more90), more75 = mean(more75), more50 = mean(more50)) %>%
+  mutate(guild="aridland")%>%
+  mutate(n=nrow(pct_pop_aridland_spp_cna))
+
+# wetland spp
+pct_pop_wetland_spp_cna <- pct_pop_wetland_spp_cna %>%
+  mutate(more90 = ifelse(sum>0.9,1,0)) %>%
+  mutate(more75 = ifelse(sum>0.75,1,0)) %>%
+  mutate(more50 = ifelse(sum>0.5,1,0))
+
+summary_wetland_spp_cna <- pct_pop_wetland_spp_cna %>%
+  summarize(more90 = mean(more90, na.rm=T), more75 = mean(more75, na.rm=T), more50 = mean(more50, na.rm=T)) %>%
+  mutate(guild="wetland")%>%
+  mutate(n=nrow(pct_pop_wetland_spp_cna))
+
+# generalist spp
+pct_pop_generalist_spp_cna <- pct_pop_generalist_spp_cna %>%
+  mutate(more90 = ifelse(sum>0.9,1,0)) %>%
+  mutate(more75 = ifelse(sum>0.75,1,0)) %>%
+  mutate(more50 = ifelse(sum>0.5,1,0))
+
+summary_generalist_spp_cna <- pct_pop_generalist_spp_cna %>%
+  summarize(more90 = mean(more90), more75 = mean(more75), more50 = mean(more50)) %>%
+  mutate(guild="generalist") %>%
+  mutate(n=nrow(pct_pop_generalist_spp_cna))
+
+# combine rows into single table
+summary_pct_pop_guild_cna <- rbind(summary_tipping_pt_spp_cna, summary_forest_spp_cna, summary_grassland_spp_cna, summary_aridland_spp_cna, summary_wetland_spp_cna, summary_generalist_spp_cna)
+
+write_csv(summary_pct_pop_guild_cna, "outputs/summary_pct_pop_guild_cna.csv")
+
+# pivot to make tidy and plot CNA results
+library(ggplot2)
+
+summary_longer_cna <- pivot_longer(summary_pct_pop_guild_cna, cols=1:3, names_to="category", values_to="pct_spp")
+
+plot_cna <- ggplot(summary_longer_cna, aes(x=guild, y=pct_spp, fill=category)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  ggtitle("Percent of spp represented within Critical Natural Assets") +
+  xlab("Guild") +
+  ylab("Percent of spp")
+plot_cna
+
