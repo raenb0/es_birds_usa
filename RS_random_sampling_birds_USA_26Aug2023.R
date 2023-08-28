a7763789be8ef139c9845b890cd3aab6604f61de
raenb0 <rachel.neugarten@gmail.com>
2023-08-28 14:01:02 -0400

    Playing with more plots

--- RS_random_sampling_birds_USA_26Aug2023.R
+++ RS_random_sampling_birds_USA_26Aug2023.R
@@ -106,29 +106,42 @@ tp_sps <- tp_sps_vars %>%
   select(species_code, sps_groups)
 
 #join tables
-result_44pct_mean_sd_biomes <- left_join(result_44pct_mean_sd, biome_sps, 
+result_44pct_mean_sd_biomes <- left_join(result_44pct_mean_sd, biome_sps,  #add habitats
                                          by=join_by(species == species_code))
-result_44pct_mean_sd_biomes_tp <- left_join(result_44pct_mean_sd_biomes, tp_sps, 
+result_44pct_mean_sd_biomes <- left_join(result_44pct_mean_sd_biomes, tp_sps,  #add TP species
                                             by=join_by(species==species_code))
-result_44pct_mean_sd_biomes_tp <- rename(result_44pct_mean_sd_biomes_tp, 
+result_44pct_mean_sd_biomes <- rename(result_44pct_mean_sd_biomes, 
                                          habitat=sps_groups.x, 
                                          tipping_pt = sps_groups.y)
 
-write_csv(result_44pct_mean_sd_biomes_tp, "outputs/random_sampling_result_44pct_mean_sd_biomes.csv")
+write_csv(result_44pct_mean_sd_biomes, "outputs/random_sampling_result_44pct_mean_sd_biomes.csv")
 
 #repeat for 37% results
-result_37pct_mean_sd_biomes <- left_join(result_37pct_mean_sd, biome_sps, 
+result_37pct_mean_sd_biomes <- left_join(result_37pct_mean_sd, biome_sps,  #add habitats
                                          by=join_by(species == species_code))
-result_37pct_mean_sd_biomes_tp <- left_join(result_37pct_mean_sd_biomes, tp_sps, 
+result_37pct_mean_sd_biomes <- left_join(result_37pct_mean_sd_biomes, tp_sps, #add TP species
                                             by=join_by(species==species_code))
-result_37pct_mean_sd_biomes_tp <- rename(result_37pct_mean_sd_biomes_tp, 
+result_37pct_mean_sd_biomes <- rename(result_37pct_mean_sd_biomes, 
                                          habitat=sps_groups.x, 
                                          tipping_pt = sps_groups.y)
 
 write_csv(result_37pct_mean_sd_biomes_tp, "outputs/random_sampling_result_37pct_mean_sd_biomes.csv")
 
-#summarize by habitat
-result_44pct_habitat <- result_44pct_mean_sd_biomes_tp %>%
+#check raster for a single species
+#plot(pct_pop_per_sp_rast, "abetow")
+
+#summarize 44% results by habitat
+#load data if necessary
+result_44pct_mean_sd_biomes <- read_csv("outputs/random_sampling_result_44pct_mean_sd_biomes.csv")
+result_44pct_habitat <- result_44pct_mean_sd_biomes %>%
+  group_by(habitat) %>%
+  summarize(avg_representation = mean(avg),
+            stdev_representation = mean(stdev)) #ask Courtney: is this right? the mean of the standard deviations of the species in that habitat group?
+
+#summarize 37% results by habitat
+#load data if necessary
+result_37pct_mean_sd_biomes <- read_csv("outputs/random_sampling_result_37pct_mean_sd_biomes.csv")
+result_37pct_habitat <- result_37pct_mean_sd_biomes %>%
   group_by(habitat) %>%
   summarize(avg_representation = mean(avg),
             stdev_representation = mean(stdev)) #ask Courtney: is this right? the mean of the standard deviations of the species in that habitat group?
@@ -139,20 +152,88 @@ plot_44pct_habitats <- ggplot(result_44pct_habitat, aes(x=habitat, y=avg_represe
   geom_bar(position=position_dodge(), stat="identity")+
   geom_errorbar(aes(ymin=avg_representation-stdev_representation, 
                     ymax=avg_representation+stdev_representation, 
-                    width=0.2))
+                    width=0.2))+
+  ggtitle("Results of random sampling of 44% of land area, by habitat group")+
+  ylim(0, 0.5)
 plot_44pct_habitats
 
+#plot 44pct result, Tipping Point species only
+result_44pct_tipping_pt <- result_44pct_mean_sd_biomes %>%
+  filter(tipping_pt=="Tipping Point")
+
+plot_44pct_tipping_pt <- ggplot(result_44pct_tipping_pt, aes(x=species, y=avg, fill=species))+
+  geom_bar(position=position_dodge(), stat="identity")+
+  geom_errorbar(aes(ymin=avg-stdev, 
+                    ymax=avg+stdev, 
+                    width=0.2))+
+  ggtitle("Results of random sampling of 44% of land area, Tipping Pt spp")+
+  ylim(0, 0.5)
+plot_44pct_tipping_pt
+
 #plot 44pct result, aridlands species only
-result_44pct_aridlands <- result_44pct_mean_sd_biomes_tp %>%
+result_44pct_aridlands <- result_44pct_mean_sd_biomes %>%
   filter(habitat=="Aridlands")
 
 plot_44pct_aridlands <- ggplot(result_44pct_aridlands, aes(x=species, y=avg, fill=species))+
   geom_bar(position=position_dodge(), stat="identity")+
   geom_errorbar(aes(ymin=avg-stdev, 
                     ymax=avg+stdev, 
-                    width=0.2))
+                    width=0.2))+
+  ggtitle("Results of random sampling of 44% of land area, Aridland spp")+
+  ylim(0, 0.5)
 plot_44pct_aridlands
 
+#plot 44pct result, wetland species only
+result_44pct_wetlands <- result_44pct_mean_sd_biomes %>%
+  filter(habitat=="Water/wetland")
+
+plot_44pct_wetlands <- ggplot(result_44pct_wetlands, aes(x=species, y=avg))+
+  geom_bar(position=position_dodge(), stat="identity")+
+  geom_errorbar(aes(ymin=avg-stdev, 
+                    ymax=avg+stdev, 
+                    width=0.2))+
+  ggtitle("Results of random sampling of 44% of land area, Wetland/Water spp")+
+  ylim(0, 0.5)
+plot_44pct_wetlands
+
+
+#plot 37pct result
+library(ggplot2)
+plot_37pct_habitats <- ggplot(result_37pct_habitat, aes(x=habitat, y=avg_representation, fill=habitat))+
+  geom_bar(position=position_dodge(), stat="identity")+
+  geom_errorbar(aes(ymin=avg_representation-stdev_representation, 
+                    ymax=avg_representation+stdev_representation, 
+                    width=0.2))+
+  ggtitle("Results of random sampling of 37% of land area, by habitat group")+
+  ylim(0, 0.5)
+plot_37pct_habitats
+
+#plot 37pct result, Tipping Point species only
+result_37pct_tipping_pt <- result_37pct_mean_sd_biomes %>%
+  filter(tipping_pt=="Tipping Point")
+
+plot_37pct_tipping_pt <- ggplot(result_37pct_tipping_pt, aes(x=species, y=avg, fill=species))+
+  geom_bar(position=position_dodge(), stat="identity")+
+  geom_errorbar(aes(ymin=avg-stdev, 
+                    ymax=avg+stdev, 
+                    width=0.2))+
+  ggtitle("Results of random sampling of 37% of land area, Tipping Pt spp")+
+  ylim(0, 0.5)
+plot_37pct_tipping_pt
+
+
+#plot 37pct result, aridlands species only
+result_37pct_aridlands <- result_37pct_mean_sd_biomes %>%
+  filter(habitat=="Aridlands")
+
+plot_37pct_aridlands <- ggplot(result_37pct_aridlands, aes(x=species, y=avg, fill=species))+
+  geom_bar(position=position_dodge(), stat="identity")+
+  geom_errorbar(aes(ymin=avg-stdev, 
+                    ymax=avg+stdev, 
+                    width=0.2))+
+  ggtitle("Results of random sampling of 37% of land area, Aridland spp")+
+  ylim(0, 0.5)
+plot_37pct_aridlands
 
 #plot 44pct result, wetland species only
 result_44pct_wetlands <- result_44pct_mean_sd_biomes_tp %>%
@@ -162,7 +243,9 @@ plot_44pct_wetlands <- ggplot(result_44pct_wetlands, aes(x=species, y=avg))+
   geom_bar(position=position_dodge(), stat="identity")+
   geom_errorbar(aes(ymin=avg-stdev, 
                     ymax=avg+stdev, 
-                    width=0.2))
+                    width=0.2))+
+  ggtitle("Results of random sampling of 37% of land area, Wetland/Water spp")+
+  ylim(0, 0.5)
 plot_44pct_wetlands
 
-plot(pct_pop_per_sp_rast, "abetow")
+
